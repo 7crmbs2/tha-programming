c076b0bdc88f3b120d9fe0df00a634082b6b7d7b
crmbs <51399053+7crmbs2@users.noreply.github.com>
2023-12-13 16:42:41 +0100

    fix scoreboard

--- progger/src/Scoreboard.c
+++ progger/src/Scoreboard.c
@@ -33,6 +33,10 @@ void scoreboard_print(Scoreboard* scoreboard_ptr)
 	// lol that next line is not needed, we already get the correct values from the struct I guess
 	// lets make it STREET_VISIBLE / 2 and then add 5-10 so that they don't touch
 	console_zeichne_rechteck(scoreboard_ptr->x, scoreboard_ptr->y, 15, 7, hpallette[4]);
-	//cursor_bewegen()
-	printf("HERE I STILL NEED TO FILL IN STUFF");
+	cursor_setze_farbe(cpallette[7]); // set cursor to white
+	cursor_setze_farbe(hpallette[0]); // set background to black
+	cursor_bewegen(scoreboard_ptr->x + 3, scoreboard_ptr->y + 2);
+	printf("Score: %i", scoreboard_ptr->score);
+	cursor_bewegen(scoreboard_ptr->x + 3, scoreboard_ptr->y + 4);
+	printf("Lives: %i", scoreboard_ptr->lives);
 }
